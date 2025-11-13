d48dcfc36b33fbc46a8233c7cf7f856e293ddd5e
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-13 23:26:08 +0100

    feat: app.js

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
+
+// Middleware per gestire JSON nel body
+app.use(express.json());
+
+//Import router
 const routerEvents = require('./routers/events.js');
 
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>Gestore Eventi</h1>
-        <div><a href="http:${host}:${port}/events" target="_blank">Lista eventi</a>
-        </div>
-        `);
-    }
-);
+// Monta le rotte /events
 app.use('/events', routerEvents);
+
+//middleware geenrico per loggare ogni richeista
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url}`);
+    next();
+});
+
+//middleware per gestire rotte non trovate
+app.use((req, res) => {
+    res.status(404).json({error: 'rotta non trovata'});
+});
+
 app.listen(port, () => {
     console.log(`server attivo su http://${host}:${port}`);
-})
+});
