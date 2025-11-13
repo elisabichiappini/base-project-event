deec00d12d2eeaede13d3dbc54ff07e7246f464e
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-13 23:10:28 +0100

    feat: router

--- routers/events.js
+++ routers/events.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const controllerEvents = require('../controllers/events.js');
+
+//index
 router.get('/', controllerEvents.index);
+
+//show
 router.get('/:id', controllerEvents.show);
+
+//store
 router.post('/', controllerEvents.store);
+
+//update
 router.put('/:id', controllerEvents.update);
+
 module.exports = router;
