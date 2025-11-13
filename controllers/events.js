153311f8d337b2f351a0d412d56106839538aadb
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-13 21:13:43 +0100

    feat: add changes in controller

--- controllers/events.js
+++ controllers/events.js
@@ -1,14 +1,22 @@
 const eventModel = require('../models/event.js');
 
+// Lista di tutti gli eventi
 const index = (req, res) => {
-    if (!error) throw new Error('errore nella lettura degli eventi');
-    res.json(error);
+    try {
+        const events = Event.getEvent();
+        if (!events) throw new Error('errore nella lettura degli eventi');
+        res.json(events);
+    } catch( error) {
+        res.status(500).json({erorr: error.message})
+    }
+    
 }
 
+// Mostra un evento specifico per ID
 const show = (req, res) => {
     try {
         const event = eventModel.getEventById(parseInt(req.params.id));
-        if (!error) {
+        if (!event) {
             return res.status(404).json({ error: 'errore non trovato' });
         }
         res.json(event);
